703f473de7a7455d30ea9bee6b36f160f461038e
mguiste <mguiste@uw.edu>
2019-12-22 14:59:22 -0800

    Added dependencies to maven

--- TheLine/src/test/java/com/BoardTest.java
+++ TheLine/src/test/java/com/BoardTest.java
@@ -3,7 +3,6 @@ package com;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.Board;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
