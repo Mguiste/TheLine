d02e33c4472a92237a6fd9a1299b9ce368605d88
mguiste <mguiste@uw.edu>
2019-12-26 12:46:56 -0800

    Started javascript files

--- TheLine/src/main/java/com/TheLine.java
+++ TheLine/src/main/java/com/TheLine.java
@@ -1,10 +1,10 @@
 package com;
 
-import com.runners.ConsoleRunner;
+import com.runners.*;
 
 public class TheLine {
     public static void main(String[] args) {
-        ConsoleRunner.run();
-        //WebsiteRunner.run();
+        //ConsoleRunner.run();
+        WebsiteRunner.run();
     }
 }
